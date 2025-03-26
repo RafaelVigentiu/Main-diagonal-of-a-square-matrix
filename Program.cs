139752cda8c1c297ecadd005c349597be58f4b76
Rafael <Rafael.gug19@gmail.com>
2025-03-26 13:35:22 +0200

    Main Diagonal

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ex6 {
+    internal class Program {
+        static void Main(string[] args) {
+            int[,] matrix = new int[5, 5];
+            int value = 1;
+
+            // Fill matrix with sequential values.
+            for (int i = 0; i < matrix.GetLength(0); i++) {
+                for (int j = 0; j < matrix.GetLength(0); j++) {
+                    matrix[i, j] = value++;
+                }
+            }
+
+            //Print diagonal elements.
+            for (int i = 0; i < matrix.GetLength(0); i++) {
+                Console.WriteLine(matrix[i, i]);
+            }
+        }
+    }
+}
